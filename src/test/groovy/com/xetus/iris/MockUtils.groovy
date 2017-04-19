28304e1ea739580eeb556a226307ee84f5a34a0d
Theophile Meneau <tmeneau@xetus.com>
2017-04-19 17:31:57 -0400

    chore(deps): update jsonrpc4j version

--- src/test/groovy/com/xetus/iris/MockUtils.groovy
+++ src/test/groovy/com/xetus/iris/MockUtils.groovy
@@ -2,6 +2,9 @@ package com.xetus.iris
 
 import static org.mockito.Mockito.when
 
+import java.lang.reflect.Type
+import java.util.Map
+
 import org.mockito.Mockito
 
 import com.googlecode.jsonrpc4j.JsonRpcHttpClient
@@ -20,31 +23,15 @@ class MockUtils {
       throw new IllegalArgumentException("Failed to locate test response file: "
         + jsonResponse)
     }
-    OutputStream os = new ByteArrayOutputStream()
-    os.write("{ \"jsonrpc\": 2.0, \"method\": \"fake\" }".getBytes("UTF-8"))
-    HttpURLConnection con = Mockito.mock(HttpURLConnection.class)
+    
     System.out.println("repsonse: " + new File(responseFile.toURI()).text)
-    /*
-     * Mock the JsonRpcHttpClient's connection so that no request is actually
-     * issued, and:
-     * 
-     *  1. replace the OutputStream with the fake request (just to avoid
-     *  internal errors, since we're not testing the output request here)
-     *  
-     *  2. replace the InputStream with the test data we want the 
-     *  JsonRpcHttpClient to deserialize for us
-     */
-    when(con.getOutputStream()).thenReturn(os)
-    when(con.getInputStream()).thenReturn(
-      MockUtils.class.getResourceAsStream(jsonResponse))
-
     JsonRpcHttpClient client = new JsonRpcHttpClient(
           ObjectMapperBuilder.getObjectMapper(),
-          new URL("http://dwww.butts.com"),
+          new URL("http://fake.url.com"),
           [:]) {
       @Override
-      protected HttpURLConnection prepareConnection(java.util.Map<String,String> extraHeaders) throws IOException {
-        return con
+      public Object invoke(String methodName, Object argument, Type returnType, Map<String, String> extraHeaders) throws Throwable {
+        return super.readResponse(returnType, MockUtils.class.getResourceAsStream(jsonResponse))
       };
     }
     
